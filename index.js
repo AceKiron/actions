bdc5bee0a077d17e8e138a89717a6171f105818b
Ace <acekiron@outlook.com>
2022-05-14 11:09:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ try {
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  console.log(process.env);
 } catch (error) {
   core.setFailed(error.message);
 }
