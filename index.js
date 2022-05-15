1e92c5ca690c69a141b961cf07b99f5025a8980d
Ace <acekiron@outlook.com>
2022-05-15 09:15:56 +0200

    Create push-to-repository action

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 
 try {
-  core.setOutput("time", new Date().toTimeString());
+    const cp = require("child_process");
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-  console.log(process.env);
+    cp.execSync("git config --local user.email \"41898282+github-actions[bot]@users.noreply.github.com\"");
+    cp.execSync("git config --local user.name \"github-actions[bot]\"");
+
+    cp.execSync(`git add ${core.getInput("files")}`);
+    cp.execSync(`git commit -m "${core.getInput("message")}" ${core.getInput("commit-flags")}`);
+    cp.execSync(`git push origin ${core.getInput("branch")} ${core.getInput("push-flags")}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
